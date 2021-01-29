bff923046e5ad3e486da57362b7b2304b0b1499a
Michael Shaleen <shaleenmichael@protonmail.com>
2021-01-29 17:56:24 -0600

    input fields made

--- salary.js
+++ salary.js
@@ -1,9 +1,29 @@
 console.log('here');
 
-let employeeInfo = {
-  employeeFirstName: '',
-  lastName: '',
-  idNumber: '',
-  jobTitle: '',
-  annualSalary: '',
+//let employeeInfo = {
+//employeeFirstName: '',
+//lastName: '',
+//idNumber: Number(''),
+//jobTitle: '',
+//annualSalary: Number(''),
+//};
+
+$('#submitButton').on('click', collector);
+
+let person1 = {
+  employeeFirstName: 'Dan',
+  lastName: 'Danson',
+  idNumber: 4,
+  jobTitle: 'Toilet Guy',
+  annualSalary: '40,000',
 };
+
+function collector(user) {}
+collector(person1);
+
+$('.form');
+
+//for (const key in user) {
+//console.log(`${key}: ${user[key]}`);
+//}
+//return true;
