86b96b4d2a9478d439dd5019a7b773b5bf437504
Michael Shaleen <shaleenmichael@protonmail.com>
2021-01-29 18:15:32 -0600

    getting lost haha

--- salary.js
+++ salary.js
@@ -1,29 +1,37 @@
 console.log('here');
-
-//let employeeInfo = {
-//employeeFirstName: '',
-//lastName: '',
-//idNumber: Number(''),
-//jobTitle: '',
-//annualSalary: Number(''),
-//};
-
-$('#submitButton').on('click', collector);
-
-let person1 = {
-  employeeFirstName: 'Dan',
-  lastName: 'Danson',
-  idNumber: 4,
-  jobTitle: 'Toilet Guy',
-  annualSalary: '40,000',
+$(document).ready(collector);
+let employeeInfo = {
+  employeeFirstName: '',
+  lastName: '',
+  idNumber: Number(''),
+  jobTitle: '',
+  annualSalary: Number(''),
 };
 
-function collector(user) {}
-collector(person1);
-
-$('.form');
-
+function collector() {
+  let inputField = [
+    $('#fname').val() +
+      $('#lname').val() +
+      $('#idNum').val() +
+      $('#jobTitle').val() +
+      $('#anSal').val(),
+  ];
+  $('#ul').append(`
+  <li>
+  info
+  </li> 
+  console.log('yo');
+  //$('.form');
+}
 //for (const key in user) {
 //console.log(`${key}: ${user[key]}`);
 //}
 //return true;
+
+//let employeeGroup= {
+//employeeFirstName: 'Dan',
+//lastName: 'Danson',
+//idNumber: 4,
+//jobTitle: 'Toilet Guy',
+//annualSalary: '40,000',
+//};
